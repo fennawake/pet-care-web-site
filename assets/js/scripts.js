2927137a95d949c370c533afe7bf303d9bbb7efa
Luiz Fernando Souza <fenna.wake@gmail.com>
2022-03-15 22:11:42 +1000

    Adjust responsive mode

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,11 +1,35 @@
 // OWL CAROUSEL
-
 $(document).ready(function () {
 	$('.owl-carousel').owlCarousel({
 		loop: false,
 		margin: 6,
 		autoWidth: false,
 		nav: true,
-		items: 2
+		items: 2,
+		responsive: {
+			0: {
+				items: 1,
+				nav: true
+			},
+			705: {
+				items: 1,
+				nav: true
+			},
+			706: {
+				items: 2,
+				nav: true
+			}
+		}
 	})
 })
+
+// MENU HAMBURGER
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+
+hamburger.addEventListener('click', mobileMenu)
+
+function mobileMenu() {
+	hamburger.classList.toggle('active')
+	navMenu.classList.toggle('active')
+}
